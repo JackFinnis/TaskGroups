5fd1d18877004b602560e4f9013901f22b0f3ae5
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-03 18:16:31 +0100

    Initial Commit

---
+++ TaskGroups/TaskGroupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGroupsApp.swift
+//  TaskGroups
+//
+//  Created by William Finnis on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TaskGroupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
